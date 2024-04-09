1cb40434d64f4c27beb0938a598a3e151795aba7
timpulsa <timpulsa@territorium.com>
2024-04-08 21:46:14 -0600

    Profile changes

--- front/app/Profile/page.js
+++ front/app/Profile/page.js
@@ -6,31 +6,36 @@ import Container from '@mui/material/Container';
 import { createTheme } from '@mui/material/styles';
 import { useTheme } from '@mui/material/styles'; //esta va ligada al provider
 import { useState } from 'react';
-import { Card,Button, Stack, StyledBadge, Badge, Avatar, IconButton} from "@mui/material";
+import { Card,Button, Stack, StyledBadge, Badge, Avatar, IconButton, VisuallyHiddenInput} from "@mui/material";
 import EditIcon from '@mui/icons-material/Edit';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 
 export default function Profile(){
     const theme = useTheme();
-  
+    const [profilePic, setProfilePic] = useState("")
+    const [profileName, setProfileName] = useState("")
+    const [profileLastName, setProfileLastName] = useState("")
+    const [profileEmail, setProfileEmail] = useState("")
+    const [profilePassword, setProfilePassword] = useState("")
+
+    const UserProfileReviewChange = (e) => setReview(e.target.value)
+
+    const SubmitProfile = ()=> {
+        console.log("editar perfil" + profileData)
+        const ProfileData = { profileData, profileData} //Lo que voy amandar al servidor
+        createReview(ReviewData)
+    }
     return(
       <>
         <Container>
 
         <Card component="main" maxWidth="xs" sx={{ p: 4, backgroundColor: theme.palette.cardBg.main}}>     
        
-       <Badge 
-           overlap="circular"
-           anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
-           badgeContent={
-               <EditIcon sx={{ borderRadius: '50%', backgroundColor: theme.palette.secondary.main}}/>
-               
-           }
-       >
+       
            <Avatar sx={{ borderRadius: '50%', width: '200px', height: '200px' }} alt="Remy Sharp" src="https://randomuser.me/api/portraits/men/60.jpg" />
-       </Badge>
-       <Box
+          
+     <Box
        component="form"
        
        sx={{
@@ -50,7 +55,7 @@ export default function Profile(){
            defaultValue="Hello World"
            variant="filled"
            fullWidth
-           
+           onChange={ setProfileName }
            />
            <TextField 
            required
@@ -59,7 +64,7 @@ export default function Profile(){
            defaultValue="Hello World"
            variant="filled"
            fullWidth
-           
+           onChange={ setProfileLastName }
            />
 
            
@@ -71,18 +76,19 @@ export default function Profile(){
            defaultValue="Hello World"
            variant="filled"
            fullWidth
-           
+           onChange={ setProfileEmail }
            />
            <TextField
            id="outlined-password-input"
            label="Password"
            type="password"
            autoComplete="current-password"
+           onChange={ setProfilePassword }
            />
            </Stack>    
            </div>
        </Box>
-       <Button variant="contained" sx={{mb: 4}}>Guardar</Button>
+       <Button variant="contained" onClick={ SubmitProfile } sx={{mb: 4}}>Guardar</Button>
        <Button variant="contained" sx={{mb: 4}}>Cancelar</Button>
       
    </Card>
