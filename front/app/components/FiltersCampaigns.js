7de402e1fda8d0b10b934f0346169af0ed69fbe0
Ratchet Cooper <slyo_ocooperrhotmail.com>
2024-06-01 22:03:23 -0600

    hay que modificar el como publica la informaicon y al hacer click lleve a pag de campaña

--- front/app/components/FiltersCampaigns.js
+++ front/app/components/FiltersCampaigns.js
@@ -1,9 +1,7 @@
 "use client"
-import {Button, Divider, Card, CardContent, Box, Typography, Stack, FormControlLabel, RadioGroup, Radio, Select, MenuItem} from "@mui/material";
+import {Button, Divider, Card, CardContent, Box, Typography, Stack, TextField, Select, MenuItem} from "@mui/material";
 import { useTheme } from '@mui/material/styles'; //esta va ligada al provider
-import TextField from '@mui/material/TextField';
 import TravelExploreRoundedIcon from '@mui/icons-material/TravelExploreRounded';
-import {createFilter} from '../services/reviewServices'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react';
 import { DatePicker } from '@mui/x-date-pickers/';
@@ -19,190 +17,172 @@ var fecha = null;
 var hora = null;
 var estrellas = null;
 
-export default function FiltersCampaigns(){
+export default function FiltersCampaigns({ onFilterSubmit }) {
     const theme = useTheme();
-    const router = useRouter()
+    const router = useRouter();
     const [filterName, setFilterName] = useState(null);
     const [filterPlayersMax, setPlayersMax] = useState(null);
     const [filterRating, setFilterRating] = useState('');
     const [selectedDate, setSelectedDate] = useState(null);
     const [selectedTime, setSelectedTime] = useState(null);
-   
-    const filterRatingChange = (e) => setFilterRating(e.target.value)
-    const filterPlayersMaxChange = (event) => setPlayersMax(event.target.value)
-    const filterNameChange = (e) => setFilterName(e.target.value)
 
-    const SubmitFilter = async ()=> {
-        try {
-          console.log(filterName,filterRating,filterPlayersMax,Fecha,Hora);
-          if( filterRating != '')
-            {
-              estrellas = filterRating;
-            }
-        const response = await fetch('http://localhost:8000/searchcampaign', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-         
-          body: JSON.stringify({ filterName: filterName, filterRating: estrellas, filterPlayersMax: filterPlayersMax, Fecha: fecha, Hora: hora  })
-        });
-        const data = await response.json();
-        console.log('resultado de select');
-        console.log(data);
-        console.log('pruebaA:');
-        console.log(data[4]);
-        console.log(data[4].Titulo);
-        if (response.ok) {
-          // Redirect to the landing page on successful login
-         // window.location.href = '/Landing'; // Redirect using window.location
-        } else {
-          // Handle error messages
-          console.error(data.message);
-        }
-      } catch (error) {
-        console.error('Error logging in:', error.message);
-      }
+    const filterRatingChange = (e) => setFilterRating(e.target.value);
+    const filterPlayersMaxChange = (event) => setPlayersMax(event.target.value);
+    const filterNameChange = (e) => setFilterName(e.target.value);
 
-
-    }
-
-    
     const dateChange = (date) => {
-        setSelectedDate(date)
-        console.log(date)
-        Fecha = date.toDate()
-        Fecha = Fecha.getFullYear()+'-' + (Fecha.getMonth()+1)+'-' + Fecha.getDate()
-        console.log(Fecha)
+        setSelectedDate(date);
+        console.log(date);
+        Fecha = date.toDate();
+        Fecha = Fecha.getFullYear() + '-' + (Fecha.getMonth() + 1) + '-' + Fecha.getDate();
+        console.log(Fecha);
         fecha = Fecha;
-        
     }
 
     const timeChange = (time) => {
-        setSelectedTime(time)
-        console.log(time)
-        Hora = time.toDate()
-        Hora = Hora.getHours()+':'+Hora.getMinutes()+':'+Hora.getSeconds()
+        setSelectedTime(time);
+        console.log(time);
+        Hora = time.toDate();
+        Hora = Hora.getHours() + ':' + Hora.getMinutes() + ':' + Hora.getSeconds();
         console.log(Hora);
         hora = Hora;
     }
 
-    
-    return(
-    <div>
-        <Typography color="textSecondary" variant="h4">Filtros</Typography>
-        
-        <Box sx={{ 
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            p: 5,
-            m:5,
-            flexDirection: "column",
-            backgroundColor: theme.palette.secondary.main
-        }}>
-            <Card sx={{ width: '100%', maxWidth: 345, backgroundColor: theme.palette.cardFilterColor.main, mt:4 }}>
-                <CardContent>
-                    <Stack direction="row" spacing={2} sx={{ display: "flex", alignItems: "center", pt: 4, pb: 4 }}>
-                        <TravelExploreRoundedIcon sx={{color:theme.palette.cardText.main}}/>
-                        <Typography color={theme.palette.cardText.main} gutterBottom variant="h5" component="div">
-                            Búsqueda por nombre de campaña
-                        </Typography>
-                    </Stack>
-                    <Divider color="textSecondary" />
-                </CardContent>
-                <Box sx={{ display: "flex", justifyContent: "center", px: 2 }}>
-                    <TextField 
-                        onChange={filterNameChange}
-                        id="outlined-search" 
-                        label="Nombre de la campaña" 
-                        type="search" 
-                        helperText="Coloca el nombre de la campaña"
-                        fullWidth
-                        sx={{ maxWidth: '100%' }}
-                    />
-                </Box>
-            </Card>
-    
-            
-            <Card sx={{ width: '100%', maxWidth: 345, backgroundColor: theme.palette.cardFilterColor.main, mt:4 }}>
-                <CardContent>
-                    <Stack direction="row" spacing={2} sx={{ display: "flex", alignItems: "center", pt: 4, pb: 4 }}>
-                        <StarBorderIcon  sx={{color:theme.palette.cardText.main}}/>
-                        <Typography color={theme.palette.cardText.main} gutterBottom variant="h5" component="div">
-                            Búsqueda por rating
-                        </Typography>
-                    </Stack>
-                    <Select 
-                        onChange={filterRatingChange}
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select-character"
-                        label="Age"
-                        fullWidth
-                        sx={{
-                            bgcolor: theme.palette.inputBackground.main, // Cambia el color de fondo del Select
-                            '&:focus': {
-                              bgcolor: 'lightblue', // Cambia el color de fondo cuando está enfocado
-                            },
-                          }}
-                    >
-                        <MenuItem value={1}>1 Estrella</MenuItem>
-                        <MenuItem value={2}>2 Estrellas</MenuItem>
-                        <MenuItem value={3}>3 Estrellas</MenuItem>
-                        <MenuItem value={4}>4 Estrellas</MenuItem>
-                        <MenuItem value={5}>5 Estrellas</MenuItem>
-                    </Select>
-                    <Divider color="textSecondary" />
-                </CardContent>
-                
-            </Card>
-            
-            <Card sx={{ width: '100%', maxWidth: 345, backgroundColor: theme.palette.cardFilterColor.main, mt:4 }}>
-                <CardContent>
-                    <Stack direction="row" spacing={2} sx={{ display: "flex", alignItems: "center", pt: 4, pb: 4 }}>
-                        <GroupsIcon sx={{color:theme.palette.cardText.main}} />
-                        <Typography color={theme.palette.cardText.main} gutterBottom variant="h5" component="div">
-                            Número máximo de jugadores
-                        </Typography>
-                    </Stack>
-                    
-                    <Divider color="textSecondary" />
-                </CardContent>
-                <Box sx={{ display: "flex", justifyContent: "center", px: 2 }}>
-                    <TextField 
-                        id="outlined-search" 
-                        onChange={filterPlayersMaxChange}
-                        label="Search field" 
-                        type="search" 
-                        helperText="Some important text"
-                        fullWidth
-                       
-                        InputProps={{
-                                    style: { color: 'white' },
-                                }}
-                               
-                                InputLabelProps={{
-                                    style: { color: 'white' },
-                                }}
-                                sx={{  mb: 2,
-                                    maxWidth: '100%',
-                                    '& .MuiOutlinedInput-root': {
-                                        '& fieldset': {
-                                            borderColor: 'white',
-                                        },
-                                        '&:hover fieldset': {
-                                            borderColor: 'white',
-                                        },
-                                        '&.Mui-focused fieldset': {
-                                            borderColor: 'white',
-                                        },
-                                    },
-                                }}
-                    />
-                </Box>
-            </Card>
+    async function SubmitFilter() {
+        try {
+            console.log(filterName, filterRating, filterPlayersMax, Fecha, Hora);
+            if (filterRating !== '') {
+                estrellas = filterRating;
+            }
+            const response = await fetch('http://localhost:8000/searchcampaign', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ filterName: filterName, filterRating: estrellas, filterPlayersMax: filterPlayersMax, Fecha: fecha, Hora: hora })
+            });
+            const data = await response.json();
+            console.log('resultado de select');
+            console.log(data);
+            if (response.ok) {
+                onFilterSubmit(data);
+            } else {
+                console.error(data.message);
+            }
+        } catch (error) {
+            console.error('Error logging in:', error.message);
+        }
+    }
 
-            <Card sx={{ width: '100%', maxWidth: 345, backgroundColor: theme.palette.cardFilterColor.main, mt: 4 }}>
+    return (
+        <div>
+            <Typography color="textSecondary" variant="h4">Filtros</Typography>
+            <Box sx={{ 
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                p: 5,
+                m:5,
+                flexDirection: "column",
+                backgroundColor: theme.palette.secondary.main
+            }}>
+                <Card sx={{ width: '100%', maxWidth: 345, backgroundColor: theme.palette.cardFilterColor.main, mt:4 }}>
+                    <CardContent>
+                        <Stack direction="row" spacing={2} sx={{ display: "flex", alignItems: "center", pt: 4, pb: 4 }}>
+                            <TravelExploreRoundedIcon sx={{color:theme.palette.cardText.main}}/>
+                            <Typography color={theme.palette.cardText.main} gutterBottom variant="h5" component="div">
+                                Búsqueda por nombre de campaña
+                            </Typography>
+                        </Stack>
+                        <Divider color="textSecondary" />
+                    </CardContent>
+                    <Box sx={{ display: "flex", justifyContent: "center", px: 2 }}>
+                        <TextField 
+                            onChange={filterNameChange}
+                            id="outlined-search" 
+                            label="Nombre de la campaña" 
+                            type="search" 
+                            helperText="Coloca el nombre de la campaña"
+                            fullWidth
+                            sx={{ maxWidth: '100%' }}
+                        />
+                    </Box>
+                </Card>
+        
+                <Card sx={{ width: '100%', maxWidth: 345, backgroundColor: theme.palette.cardFilterColor.main, mt:4 }}>
+                    <CardContent>
+                        <Stack direction="row" spacing={2} sx={{ display: "flex", alignItems: "center", pt: 4, pb: 4 }}>
+                            <StarBorderIcon  sx={{color:theme.palette.cardText.main}}/>
+                            <Typography color={theme.palette.cardText.main} gutterBottom variant="h5" component="div">
+                                Búsqueda por rating
+                            </Typography>
+                        </Stack>
+                        <Select 
+                            onChange={filterRatingChange}
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select-character"
+                            label="Rating"
+                            fullWidth
+                            sx={{
+                                bgcolor: theme.palette.inputBackground.main,
+                                '&:focus': {
+                                  bgcolor: 'lightblue',
+                                },
+                              }}
+                        >
+                            <MenuItem value={1}>1 Estrella</MenuItem>
+                            <MenuItem value={2}>2 Estrellas</MenuItem>
+                            <MenuItem value={3}>3 Estrellas</MenuItem>
+                            <MenuItem value={4}>4 Estrellas</MenuItem>
+                            <MenuItem value={5}>5 Estrellas</MenuItem>
+                        </Select>
+                        <Divider color="textSecondary" />
+                    </CardContent>
+                </Card>
+                
+                <Card sx={{ width: '100%', maxWidth: 345, backgroundColor: theme.palette.cardFilterColor.main, mt:4 }}>
+                    <CardContent>
+                        <Stack direction="row" spacing={2} sx={{ display: "flex", alignItems: "center", pt: 4, pb: 4 }}>
+                            <GroupsIcon sx={{color:theme.palette.cardText.main}} />
+                            <Typography color={theme.palette.cardText.main} gutterBottom variant="h5" component="div">
+                                Número máximo de jugadores
+                            </Typography>
+                        </Stack>
+                        <Divider color="textSecondary" />
+                    </CardContent>
+                    <Box sx={{ display: "flex", justifyContent: "center", px: 2 }}>
+                        <TextField 
+                            id="outlined-search" 
+                            onChange={filterPlayersMaxChange}
+                            label="Número máximo de jugadores" 
+                            type="search" 
+                            helperText="Número máximo de jugadores"
+                            fullWidth
+                            InputProps={{
+                                style: { color: 'white' },
+                            }}
+                            InputLabelProps={{
+                                style: { color: 'white' },
+                            }}
+                            sx={{  mb: 2,
+                                maxWidth: '100%',
+                                '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {
+                                        borderColor: 'white',
+                                    },
+                                    '&:hover fieldset': {
+                                        borderColor: 'white',
+                                    },
+                                    '&.Mui-focused fieldset': {
+                                        borderColor: 'white',
+                                    },
+                                },
+                            }}
+                        />
+                    </Box>
+                </Card>
+
+                <Card sx={{ width: '100%', maxWidth: 345, backgroundColor: theme.palette.cardFilterColor.main, mt: 4 }}>
                     <CardContent>
                         <Stack direction="row" spacing={2} sx={{ display: "flex", alignItems: "center", pt: 4, pb: 4 }}>
                             <CalendarMonthIcon sx={{ color: theme.palette.cardText.main }} />
@@ -237,17 +217,17 @@ export default function FiltersCampaigns(){
                     </Box>
                 </Card>
 
-            <Box sx={{ 
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                p: 5,
-                flexDirection: "column",
-                backgroundColor: theme.palette.secondary.main
-            }}>
-                <Button variant="contained" sx={{ mb: 4 }} onClick={ SubmitFilter }>Buscar</Button>
+                <Box sx={{ 
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    p: 5,
+                    flexDirection: "column",
+                    backgroundColor: theme.palette.secondary.main
+                }}>
+                    <Button variant="contained" sx={{ mb: 4 }} onClick={SubmitFilter}>Buscar</Button>
+                </Box>
             </Box>
-        </Box>
-    </div>
-    )
+        </div>
+    );
 }
